f4fdab57379bbb7e3616bef10e9b1b7b5fe5200e
Kasra Kassai <KasraK@Kasras-MacBook-Pro.local>
2016-02-09 23:38:06 +0330

    3 test routes addedwq

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const http = require('http');
-server = http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type":"text/plain"});
-	response.write("hello world");
-	response.end();
+var express = require('express');
+var app = express();
+
+app.get('/', function (request, response) {
+	response.send('<h1>hello</h1> express');
 });
-server.listen(3000);
+
+
+app.get('/me', function (request, response) {
+	response.send('@seekasra');
+});
+
+var server = app.listen(3000, function (){
+	console.log('Listening on port 3000');
+});
+
+
+app.get('/who/:name?', function (request, response) {
+	var name = request.params.name;
+	response.send(name +' was here');
+});
+
